3ee316c6fbb021521f9613e34ec791689f8795ba
drewejohnson <1drew.e.johnson@gmail.com>
2017-06-11 15:39:00 -0700

    Plotting with pandas - WIP

--- main.py
+++ main.py
@@ -4,21 +4,36 @@ Main
 Main script to plot my coffee intake 
 """
 
+import numpy
 from matplotlib import pyplot
+import pandas
 
-import plotter
+import utils
 
 defaultScheme = 'bright'
 
 figArgs = {'figsize': (16.0, 9.0)}
 
-data = plotter.getData()
+data = pandas.read_csv('coffee-tracker.csv', parse_dates=[[0, 1, 2, 3, 4]], header=None,
+                       date_parser=utils.dateParser)
+data.columns = ['Datetime', 'Drink', 'Location', 'City', 'State']
+data['date'] = pandas.Series([xx.date() for xx in data.Datetime])
+data['dateNum'] = pandas.Series([(xx.date() - data.Datetime[0].date()).days for xx in data.Datetime])
+data['timeNum'] = pandas.Series([xx.hour + xx.minute / 60. for xx in data.Datetime])
 
-daysVsTypeFig, dayVsTimeAx = pyplot.subplots(1, 1, **figArgs)
-plotter.plotDOYvsTime(dayVsTimeAx, data, colorBy='base', colorScheme=defaultScheme)
+uniqueDrinks = data.Drink.unique()
+data['drinkID'] = pandas.Series([numpy.where(xx == uniqueDrinks)[0][0] for xx in data.Drink])
 
-dayVsNumFig, dayVsNumAx = pyplot.subplots(1, 1, **figArgs)
-plotter.plotNumberDrinksPerDay(dayVsNumAx, data)
+# plot total drink consumption
+dayFig, dayAx = pyplot.subplots(1, 1, **figArgs)
+data.plot.scatter('dateNum', 'timeNum', ax=dayAx)
+utils.formatAxis(dayAx, x='date', y='time')
+
+# plot number of drinks per day
+numDrinksFig, numDrinksAx = pyplot.subplots(1, 1, **figArgs)
+data.dateNum.value_counts().plot.hist(ax=numDrinksAx)
+numDrinksAx.set_xlabel('Number of drinks per day')
+numDrinksAx.set_ylabel('Frequency')
 
 if __name__ == '__main__':
     pyplot.show()
--- utils.py
+++ utils.py
@@ -1,71 +1,23 @@
 """
+Utilities for creating/plotting data
 
+"""
 
-"""             
-import csv
+from datetime import datetime
 
-import Drinks
-
-
-drinks = {
-    'drip': Drinks.Drip, 'espresso': Drinks.Espresso, 'mocha': Drinks.Mocha,
-    'pour over': Drinks.PourOver, 'cold brew': Drinks.ColdBrew,  'latte': Drinks.Latte, 
-    'siphon': Drinks.Siphon,  'french press': Drinks.FrenchPress, 
-    'four sigmatic think': Drinks.FourSigThink, 'cappuccino': Drinks.Cappuccino,
-    'flat white': Drinks.FlatWhite, 'aeropress': Drinks.Aeropress,
-    'iced coffee': Drinks.IcedCoffee}
-
-def csvToList(filePath, formatFunc):
-    """Return the data from keepFile filePath."""
-    data = []
-    with open(filePath, 'r', newline='') as csvObj:
-        reader = csv.reader(csvObj, delimiter=',')
-        for line in reader:
-            data.append(formatFunc(cleanLine(line)))
-    return data
+def dateParser(*args):
+    return datetime.strptime('-'.join([str(xx) for xx in args]), '%y-%m-%d-%H-%M')
     
-
-def formatKeepLine(line, defaultCity=('Atlanta', 'GA')):
-    """Reformat the data in keep formatted line."""
-    addDefaultCity(line, 8, defaultCity)
-    if line[5] == 'pm':
-        line[3] = str(int(line[3]) + 12)
-    del line[5] 
-    return line     
+def formatAxis(ax, x='date', y='time'):
+    funcs = {'date': _formatAxis_date, 'time': _formatAxis_time}
+    if x in funcs:
+        funcs[x](ax)
+    if y in funcs:
+        funcs[y](ax)            
     
-
-def formatDocsLine(line, defaultCity=('Atanta', 'GA')):
-    """Reformat the data from the original Google Docs format."""
-    line[5], line[6] = line[6], line[5]
-    addDefaultCity(line, 7, defaultCity)
-    return line
+def _formatAxis_date(ax):
+    ax.set_xlabel('Date')
     
-
-def createObjects(line):
-    """Return an instance of Drink corresponding to the data in this line."""
-    dateTime = line[:5]
-    location = line[6:]
-    drinkType = line[5]
-    if drinkType in drinks:
-        return drinks[drinkType](dateTime, location)
-    else:
-        raise KeyError('drink {} not in drink dictionary\n{}'.format(drinkType, line))       
-    
-def cleanLine(lineList):
-    return [val.strip() for val in lineList if val]
-
-
-def addDefaultCity(lineList, shortLen, defaultCity):
-    if len(lineList) == shortLen:
-        lineList.extend(defaultCity)
-
-
-def _writecsv(outFile, data):
-    with open(outFile, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',')
-        for coffeeObj in data:
-            writer.writerow(coffeeObj.toCsv())
-
-def writeListOfDrinks(outFile, data):
-    writeData = sorted(data, key=lambda coffeeObj: coffeeObj.dateTime)
-    _writecsv(outFile, writeData)
+def _formatAxis_time(ax):
+    ax.set_ylabel('Local Time')        
+    # more methods for setting ticks
